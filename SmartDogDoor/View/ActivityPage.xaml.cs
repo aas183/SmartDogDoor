5455676445c0839df3a21a152b87bafd3c4515e8
aas183 <aas183@uakron.edu>
2024-02-20 11:16:48 -0500

    Add Filtering Pets to Pet Activity Table

--- SmartDogDoor/View/ActivityPage.xaml.cs
+++ SmartDogDoor/View/ActivityPage.xaml.cs
@@ -2,9 +2,35 @@
 
 public partial class ActivityPage : ContentPage
 {
+    ActivityViewModel ViewModel;
      public ActivityPage(ActivityViewModel viewModel)
      {
          InitializeComponent();
+        // ViewModel = viewModel;
          BindingContext = viewModel;
      }
+
+    
+    void OnTypePickerSelectedIndexChanged(object sender, EventArgs e)
+    {
+        var picker = (Picker)sender;
+        int selectedIndex = picker.SelectedIndex;
+
+        if (selectedIndex == 0)
+        {
+            petPicker.IsVisible = false;
+
+        }
+        else
+        {
+            petPicker.IsVisible = true;
+        }
+    }
+
+    /*
+    void OnPetPickerSelectedIndexChanged(object sender, EventArgs e)
+    {
+        //ViewModel.filterActivity();
+    }
+    */
 }
--- SmartDogDoor/ViewModel/ActivityViewModel.cs
+++ SmartDogDoor/ViewModel/ActivityViewModel.cs
@@ -7,10 +7,40 @@ public partial class ActivityViewModel : BaseViewModel
 {
     PetService petService;
     public ObservableCollection<PetActivity> Activities { get; } = new();
+    public ObservableCollection<PetActivity> FilteredActivities { get; } = new();
+    public ObservableCollection<Pet> Pets { get; } = new();
+    private Pet _selectedPet;
+    public Pet SelectedPet
+    {
+        get
+        {
+            return _selectedPet;
+        }
+        set
+        {
+            SetProperty(ref _selectedPet, value);
+            filterActivity();
+        }
+    }
+    private int _selectedFilterIndex = 0;
+    public int SelectedFilterIndex
+    {
+        get
+        {
+            return _selectedFilterIndex;
+        }
+        set
+        {
+            _selectedFilterIndex = value;
+            filterActivity();
+        }
+    }
+
     public ActivityViewModel(PetService petService)
     {
         Title = "Activity";
         this.petService = petService;
+        GetPetsAsync();
     }
 
 
@@ -25,11 +55,15 @@ public partial class ActivityViewModel : BaseViewModel
             var activities = await petService.GetPetActivities();
             
             if(Activities.Count != 0)
+            {
                 Activities.Clear();
+                FilteredActivities.Clear();
+            }
 
             foreach (var activity in activities)
             {
                 Activities.Add(activity);
+                FilteredActivities.Add(activity);
             }
                
         }
@@ -45,9 +79,67 @@ public partial class ActivityViewModel : BaseViewModel
         }
     }
 
-    /*
-    async Task filterActivity(bool time_pet, string petID)
+    //Get Details from pet Information Page
+    [RelayCommand]
+    async Task GetPetsAsync()
     {
+        //If data pull is already occurring quit
+        if (IsBusy) return;
+
+
+        try
+        {
+            IsBusy = true;
+            var pets = await petService.GetPets();//Get pets
+
+            if (Pets.Count != 0)//clear pet list if full
+                Pets.Clear();
+
+            foreach (var pet in pets)//update pet list from service call
+            {
+                Pets.Add(pet);
+            }
+        }
+        catch (Exception ex)//if error
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!",
+                $"Unable to get pets: {ex.Message}", "OK");
+        }
+        finally
+        {
+            IsBusy = false;//set busy back to false
+        }
+    }
+
+    //Get Details from pet Information Page
+    //[RelayCommand]
+    public void filterActivity()
+    {
+        if(SelectedFilterIndex == 0)
+        {
+            FilteredActivities.Clear();
+            foreach (var activity in Activities)
+            {
+                FilteredActivities.Add(activity);
+            }
+        }
+        else
+        {
+            if(_selectedPet != null)
+            {
+                var selectedPetId = _selectedPet.Id;
+                FilteredActivities.Clear();
+                foreach (var activity in Activities)
+                {
+                    if (activity.Id == selectedPetId)
+                    {
+                        FilteredActivities.Add(activity);
+                    }
+
+                }
+            }
+        }
         //call pet service, getPetActivities(), to update Activities observable collection
 
         //check time_pet bool
@@ -57,5 +149,4 @@ public partial class ActivityViewModel : BaseViewModel
             //look through observable collection and delete entries that do not have the passed petID
             //sort entries by most recent activity first
     }
-    */
 }
